e04e075f0a94a887a1c38d7b4584f3ac30cd4dbe
Matheus Oliveira <ma.pe.oli.1998@gmail.com>
2026-07-19 23:09:27 -0300

    FIZ WAR

--- backend/src/main/java/br/com/matheus/hubfeatcreators/HubFeatCreatorsApplication.java
+++ backend/src/main/java/br/com/matheus/hubfeatcreators/HubFeatCreatorsApplication.java
@@ -2,14 +2,21 @@ package br.com.matheus.hubfeatcreators;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
 @EnableScheduling
-public class HubFeatCreatorsApplication {
+public class HubFeatCreatorsApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(HubFeatCreatorsApplication.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(HubFeatCreatorsApplication.class, args);
     }
